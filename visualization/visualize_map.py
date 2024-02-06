fa6c2437d476037f7df92e1bf70ce109a0b8db80
lejames <okejames55@gmail.com>
2024-02-07 00:04:23 +0100

    Update display map function to use the FoliumMapManager class

--- visualization/visualize_map.py
+++ visualization/visualize_map.py
@@ -1,10 +1,12 @@
 import folium
-from .folium_map import create_map
+from .folium_map import FoliumMapManager
+
+folium_map = FoliumMapManager()
 
 def display_map():
     """
     Display an interactive map centered on Münster
     """
 
-    munster_map = create_map()
+    munster_map = folium_map.create_map()
     munster_map.show_in_browser()
