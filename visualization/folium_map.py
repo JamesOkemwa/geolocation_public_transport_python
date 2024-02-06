e145a2d45808a355c04a276151258f2d3200db4d
lejames <okejames55@gmail.com>
2024-02-06 23:55:51 +0100

    Create a class that instanciates and returns the folium map object

--- visualization/folium_map.py
+++ visualization/folium_map.py
@@ -10,28 +10,45 @@ def load_bus_stops():
     stops_df = pd.read_csv("data/gtfs data/stadtwerke_feed/stops.txt")
     return stops_df
 
-def create_map():
-    """
-    Create folium map centered on Münster and add markers for all bus stops
-    """
 
-    m = folium.Map(location=(51.96, 7.62), tiles="cartodb positron", zoom_start=12)
+class FoliumMapManager:
+    def __init__(self):
+        self.munster_coordinates = [51.96, 7.62]
+        self.map_instance = None
+        self.user_position = None
 
-    marker_cluster = MarkerCluster(
-        name='1000 clustered icons',
-        overlay=True,
-        control=False,
-        icon_create_function=None
-    )
+    def on_location_found(self, e):
+        """
+        Callback function for when the user location is found
+        """
+        self.user_position = [e.latitude, e.longitude]
+        return self.user_position
 
-    stops_df = load_bus_stops()
-    for index, bus_stop in stops_df.iterrows():
-        bus_stop_coords = [bus_stop['stop_lat'], bus_stop['stop_lon']]
-        marker = folium.Marker(location=bus_stop_coords, popup=bus_stop['stop_name']).add_to(marker_cluster)
+    def create_map(self):
+        """
+        Create folium map centered on Münster and add markers for all bus stops
+        """
 
-    marker_cluster.add_to(m)
+        if self.map_instance is None:
+            self.map_instance = folium.Map(location=self.munster_coordinates, tiles="cartodb positron", zoom_start=12)
 
-    # add geolocation control
-    geolocation = LocateControl(auto_start=True).add_to(m)
+            marker_cluster = MarkerCluster(
+                name='bus stops',
+                overlay=True,
+                control=False,
+                icon_create_function=None
+            )
 
-    return m
+            stops_df = load_bus_stops()
+            for index, bus_stop in stops_df.iterrows():
+                bus_stop_coords = [bus_stop['stop_lat'], bus_stop['stop_lon']]
+                marker = folium.Marker(location=bus_stop_coords, popup=bus_stop['stop_name'])
+                marker_cluster.add_child(marker)
+
+            marker_cluster.add_to(self.map_instance)
+
+            # add geolocation control
+            geolocation = LocateControl(auto_start=True)
+            self.map_instance.add_child(geolocation)
+
+        return self.map_instance
